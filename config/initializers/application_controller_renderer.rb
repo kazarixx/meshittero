2a88fe3cdf9192ead39b901c97abd492b85764a1
kazarixx <tua.ih8@gmail.com>
2021-09-15 10:33:02 +0000

    [finish]アプリケーション作成②　３章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'efe182239c180fa620e37ea4732989b0f8f5967537b71f1c1ff67f25eac6f85d39980baeb3c94d8e0f6caffdfb812467c9fe35538138af49a1f2cb2237a96eaf'
